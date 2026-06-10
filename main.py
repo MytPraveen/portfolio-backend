cb2331121cb92b4404d42e8ac8488980909da97c
MytPraveen <mosespraveen96@gmail.com>
2026-06-10 19:58:13 +0530

    Updated the python main.py file with new extra code

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {
-        "message": "Portfolio Backend Running"
-    }
+    return {"message": "Portfolio Backend Running"}
+
+@app.get("/projects")
+def projects():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        database="portfolio_db",
+        user="portfolio_user",
+        password="YOUR_PASSWORD"
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT id,title,description
+        FROM projects
+    """)
+
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return {"projects": rows}
